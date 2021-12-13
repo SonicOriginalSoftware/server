1458627570c45c6625290ac3db12b9b313bea8d3
Nathan Blair <nathanblair@silver.lan>
2021-12-12 20:31:45 -0500

    still refactoring to separate concerns

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,10 +1,34 @@
 use core::fmt;
-use std::path::PathBuf;
 
 pub struct App<'a> {
     pub name: &'a str,
     pub version: &'a str,
-    pub path: PathBuf,
+    pub path: String,
+}
+
+impl<'a> App<'a> {
+    fn path() -> String {
+        const DEFAULT_SERVE_DIRECTORY: &str = "public";
+
+        match std::env::var("PWA_PATH") {
+            Ok(p) => p,
+            Err(e) => {
+                // FIXME Fallback first to an argument passed in the command line
+                // THEN default to a "public" folder
+                println!("{}", e);
+                println!("Defaulting to {}...", DEFAULT_SERVE_DIRECTORY);
+                DEFAULT_SERVE_DIRECTORY.to_string()
+            }
+        }
+    }
+
+    pub fn new() -> App<'a> {
+        App {
+            name: env!("CARGO_PKG_NAME"),
+            version: env!("CARGO_PKG_VERSION"),
+            path: App::path(),
+        }
+    }
 }
 
 impl fmt::Display for App<'_> {
@@ -12,9 +36,7 @@ impl fmt::Display for App<'_> {
         write!(
             f,
             "\nApp name: {}\nApp version: {}\nApp path: {}",
-            self.name,
-            self.version,
-            self.path.display()
+            self.name, self.version, self.path
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use std::path::Path;
+use actix_web::{middleware, App, HttpServer};
 
 use pwa_server::{
     api::route as api_route,
@@ -7,46 +7,23 @@ use pwa_server::{
     auth::route as auth_route,
 };
 
-// TODO The below main function body needs the port to be configurable
-// And once that is done, it can be abstracted into a shared module
-// Each component will pretty much only use these scopes, and so the
-// abstracted-out function should accept callbacks to handlers for
-// these scopes
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // FIXME Make the scheme, host, port, and app path configurable based on env variables
-    // and/or arguments in the executable
-
     // TODO Server should be running on HTTP/2 over SSL/TLS
 
     const SCHEME: &str = "http";
     const HOST: &str = "localhost";
     const PORT: u32 = 8080;
 
-    let pwa_path: String = match std::env::var("PWA_PATH") {
-        Ok(p) => p,
-        Err(e) => {
-            eprintln!("{}", e);
-            String::from("public")
-        }
-    };
-
-    let app_state = AppState {
-        name: env!("CARGO_PKG_NAME"),
-        version: env!("CARGO_PKG_VERSION"),
-        path: Path::new(&pwa_path).to_path_buf(),
-    };
+    let app_state = AppState::new();
 
     println!("{}", app_state);
 
     println!("Starting server on {}://{}:{}...", SCHEME, HOST, PORT);
 
-    use actix_web::{middleware, App, HttpServer};
-
     // FIXME Logging not working
 
-    let app_data = web::Data::new(app_state);
+    // let app_data = web::Data::new(app_state);
 
     HttpServer::new(move || {
         App::new()
@@ -55,9 +32,9 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::NormalizePath::new(
                 middleware::normalize::TrailingSlash::MergeOnly,
             ))
-            .app_data(app_data.clone())
+            .app_data(web::Data::new(app_state))
             .route("/", web::get().to(root))
-            .route(pwa_path.as_str(), web::get().to(app_route))
+            .route(&app_state.path, web::get().to(app_route))
             .route("/{filename:.*}", web::get().to(app_route))
             .service(web::resource("/api").to(api_route))
             .service(web::resource("/auth").to(auth_route))
