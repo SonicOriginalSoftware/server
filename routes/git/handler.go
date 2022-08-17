b3668af90ee216c48aeb36e073da8b33719f8ec1
Nathan Blair <me@nathanblair.rocks>
2022-08-17 10:23:47 -0400

    it appears I can now push as well

--- routes/git/handler.go
+++ routes/git/handler.go
@@ -5,6 +5,8 @@ package git
 import (
 	"api-server/lib/net/env"
 	"api-server/lib/net/local"
+	"context"
+	"io"
 	"strings"
 
 	"fmt"
@@ -19,8 +21,8 @@ import (
 
 const prefix = "git"
 const infoRefsService = "refs"
-const receiveService = "receive-pack"
-const uploadService = "upload-pack"
+const receiveService = "git-receive-pack"
+const uploadService = "git-upload-pack"
 
 // Handler handles Git requests
 type Handler struct {
@@ -36,6 +38,54 @@ func (handler *Handler) handleError(writer http.ResponseWriter, errCode int, err
 	http.Error(writer, err.Error(), errCode)
 }
 
+func (handler *Handler) uploadPack(
+	context context.Context,
+	session transport.Session,
+	body io.ReadCloser,
+	writer http.ResponseWriter,
+) (err error) {
+	uploadPackRequest := packp.NewUploadPackRequest()
+	if err = uploadPackRequest.Decode(body); err != nil {
+		return
+	}
+
+	uploadPackSession, ok := session.(transport.UploadPackSession)
+	if !ok {
+		err = fmt.Errorf("Could not create upload-pack session")
+		return
+	}
+
+	uploadResponse, err := uploadPackSession.UploadPack(context, uploadPackRequest)
+	if err != nil {
+		return
+	}
+
+	return uploadResponse.Encode(writer)
+}
+
+func (handler *Handler) receivePack(
+	context context.Context,
+	session transport.Session,
+	body io.ReadCloser,
+	writer http.ResponseWriter,
+) (err error) {
+	receivePackRequest := packp.NewReferenceUpdateRequest()
+	receivePackRequest.Decode(body)
+
+	receivePackSession, ok := session.(transport.ReceivePackSession)
+	if !ok {
+		err = fmt.Errorf("Could not create receive-pack session")
+		return
+	}
+
+	reportStatus, err := receivePackSession.ReceivePack(context, receivePackRequest)
+	if err != nil || reportStatus == nil {
+		return
+	}
+
+	return reportStatus.Encode(writer)
+}
+
 // ServeHTTP fulfills the http.Handler contract for Handler
 func (handler *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Add("Cache-Control", "no-cache")
@@ -45,14 +95,23 @@ func (handler *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Requ
 
 	pathParts := strings.Split(path, "/")
 	service := pathParts[len(pathParts)-1]
+	isInfoRefsRequest := service == infoRefsService
 
-	if service != receiveService && service != uploadService && service != infoRefsService {
+	if service != receiveService && service != uploadService && !isInfoRefsRequest {
 		handler.handleError(writer, http.StatusForbidden, err)
 		return
 	}
 
-	serviceQuery := request.URL.Query().Get("service")
-	repoPath := strings.Join(pathParts[0:len(pathParts)-2], "/")
+	contentTypeSuffix := "result"
+	trimParts := 1
+
+	if isInfoRefsRequest {
+		trimParts = 2
+		contentTypeSuffix = "advertisement"
+		service = request.URL.Query().Get("service")
+	}
+
+	repoPath := strings.Join(pathParts[0:len(pathParts)-trimParts], "/")
 	endpoint := fmt.Sprintf("%v://%v%v", "https", request.Host, repoPath)
 	transportEndpoint, err := transport.NewEndpoint(endpoint)
 	if err != nil {
@@ -60,30 +119,26 @@ func (handler *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Requ
 		return
 	}
 
-	switch service {
-	case infoRefsService:
-		writer.Header().Set("Content-Type", fmt.Sprintf("application/x-%v-advertisement", serviceQuery))
-		var session transport.UploadPackSession
-		if session, err = handler.server.NewUploadPackSession(transportEndpoint, nil); err == nil {
-			var refs *packp.AdvRefs
-			if refs, err = session.AdvertisedReferencesContext(request.Context()); err == nil {
-				refs.Prefix = [][]byte{[]byte("# service=git-upload-pack"), pktline.Flush}
-				err = refs.Encode(writer)
-			}
+	writer.Header().Set("Content-Type", fmt.Sprintf("application/x-%v-%v", service, contentTypeSuffix))
+
+	var session transport.Session
+	if service == receiveService {
+		session, err = handler.server.NewReceivePackSession(transportEndpoint, nil)
+		if !isInfoRefsRequest && err == nil {
+			err = handler.receivePack(request.Context(), session, request.Body, writer)
 		}
-	case receiveService:
-		writer.Header().Set("Content-Type", fmt.Sprintf("application/x-%v-result", service))
-		var session transport.ReceivePackSession
-		if session, err = handler.server.NewReceivePackSession(transportEndpoint, nil); err == nil {
-			receivePackRequest := packp.NewReferenceUpdateRequest()
-			session.ReceivePack(request.Context(), receivePackRequest)
+	} else if service == uploadService {
+		session, err = handler.server.NewUploadPackSession(transportEndpoint, nil)
+		if !isInfoRefsRequest && err == nil {
+			err = handler.uploadPack(request.Context(), session, request.Body, writer)
 		}
-	case uploadService:
-		writer.Header().Set("Content-Type", fmt.Sprintf("application/x-%v-result", service))
-		var session transport.UploadPackSession
-		if session, err = handler.server.NewUploadPackSession(transportEndpoint, nil); err == nil {
-			uploadPackRequest := packp.NewUploadPackRequest()
-			session.UploadPack(request.Context(), uploadPackRequest)
+	}
+
+	if isInfoRefsRequest && err == nil {
+		var refs *packp.AdvRefs
+		if refs, err = session.AdvertisedReferencesContext(request.Context()); err == nil {
+			refs.Prefix = [][]byte{[]byte(fmt.Sprintf("# service=%v", service)), pktline.Flush}
+			err = refs.Encode(writer)
 		}
 	}
 
