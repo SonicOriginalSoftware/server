71c7e1b02cb370b640effd372dd8f8763d422ce5
Nathan Blair <me@nathanblair.rocks>
2022-08-19 23:44:12 -0400

    added the grpc handler as a default service

--- main.go
+++ main.go
@@ -9,17 +9,20 @@ import (
 	"server/routes/app"
 	"server/routes/git"
 	"server/routes/graphql"
+	"server/routes/grpc"
 )
 
 func main() {
 	appHandler := app.NewHandler()
 	gitHandler := git.NewHandler()
 	graphqlHandler := graphql.NewHandler()
+	grpcHandler := grpc.NewHandler()
 
 	subdomains := []handlers.SubdomainHandler{
 		appHandler,
 		gitHandler,
 		graphqlHandler,
+		grpcHandler,
 	}
 
 	defer func() { os.Exit(lib.Run(subdomains)) }()
